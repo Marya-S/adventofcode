114c1031f289c726a4a4f5779f62c12549a997f6
mariastepanova <manunica@rambler.ru>
2024-11-12 03:49:41 +0100

    fix function and test

--- src/main/java/day1/Main.java
+++ src/main/java/day1/Main.java
@@ -1,16 +1,37 @@
-package org.example;
+package day1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println("Task 01. Put in brackets");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String brackets = reader.readLine();
-        returnFloor(brackets);
         System.out.println("Floor: " + returnFloor(brackets));
+
+        System.out.println("Task 02. Put in parameters");
+        System.out.println("Put in length: ");
+        int l = Integer.parseInt(reader.readLine());
+        System.out.println("Put in width: ");
+        int w = Integer.parseInt(reader.readLine());
+        System.out.println("Put in height: ");
+        int h = Integer.parseInt(reader.readLine());
+        System.out.println("Square = " +returnSquare(l,w,h));
+
+    }
+
+    public static int returnSquare(int l, int w, int h) {
+        if(l<0 || w<0 || h<0){
+            throw new IllegalArgumentException("Incorrect parameters");
+        }
+        int[] nums = new int[] {l*w, w*h, h*l};
+        int min = Arrays.stream(nums).min().getAsInt();
+        return (2*l*w + 2*w*h + 2*h*l) + min;
+
     }
 
     public static long returnFloor(String brackets){
--- src/test/java/day1/MainTest.java
+++ src/test/java/day1/MainTest.java
@@ -1,21 +1,31 @@
-package org.example;
+package day1;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class MainTest {
-    @Test
-    void correctFloorNum() {
-        String brackets = "(())))";
-        Assertions.assertEquals(-2, Main.returnFloor(brackets), "The result is wrong");
+    @ParameterizedTest
+    @CsvFileSource(resources = "/floorData.csv", numLinesToSkip = 1)
+    void correctFloorNum(String brackets, int expectedResult) {
+        Assertions.assertEquals(expectedResult, Main.returnFloor(brackets), "The result is wrong");
     }
 
-    @Test
-    void errorInString() {
-        String brackets = "()((({((()";
+    @ParameterizedTest
+    @CsvFileSource(resources = "/errorFloorData.csv", numLinesToSkip = 1)
+    void errorInString(String brackets) {
         Assertions.assertThrows(IllegalArgumentException.class, () -> Main.returnFloor(brackets));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/squareData.csv", numLinesToSkip = 1)
+    void correctSquareData(int l, int w, int h, int expectedResult) {
+        Assertions.assertEquals(expectedResult, Main.returnSquare(l,w,h));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/errorSquareData.csv", numLinesToSkip = 1)
+    void errorParameters(int l, int w, int h) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Main.returnSquare(l,w,h));
     }
 }
